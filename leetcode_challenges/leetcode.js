f103fef20dfcbbac3bc7bc8f564f3cb0d8392a01
Samantha Wu <samanthayywu@gmail.com>
2022-04-01 00:04:26 +0100

    roman to int solution

--- leetcode_challenges/leetcode.js
+++ leetcode_challenges/leetcode.js
@@ -57,3 +57,30 @@ C can be placed before D (500) and M (1000) to make 400 and 900.
 Given a roman numeral, convert it to an integer.
 
 */
+var romanToInt = function(s) {
+    
+    let symb = {
+        "I" : 1,
+        "V" : 5,
+        "X" : 10,
+        "L" : 50,
+        "C" : 100,
+        "D" : 500,
+        "M" : 1000
+    }
+    
+    
+    let val = 0;
+    
+    for(let i = 0; i < s.length; i++){
+        if(symb[s[i]] < symb[s[i+1]]){
+            val -= symb[s[i]]; 
+        }
+        else{
+            val += symb[s[i]];
+        }
+    }
+
+  return val; 
+    
+};
