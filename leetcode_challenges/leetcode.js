ccbab5198c3673f28704e189c9aa4e4850174fa0
Samantha Wu <samanthayywu@gmail.com>
2022-04-02 09:24:10 +0100

    Palindrome Linked List

--- leetcode_challenges/leetcode.js
+++ leetcode_challenges/leetcode.js
@@ -86,3 +86,22 @@ var romanToInt = function(s) {
   return val; 
     
 };
+
+/* 
+Given the head of a singly linked list, return true if it is a palindrome.
+
+ 
+
+Example 1:
+
+
+Input: head = [1,2,2,1]
+Output: true
+Example 2:
+
+
+Input: head = [1,2]
+Output: false
+
+
+*/
