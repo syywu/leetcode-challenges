6949a9734091f0c3216260deba7a9e26c4cd7397
Samantha Wu <samanthayywu@gmail.com>
2022-04-03 23:33:29 +0100

    ocean view

--- leetcode_challenges/leetcode.js
+++ leetcode_challenges/leetcode.js
@@ -28,7 +28,7 @@ var twoSum = function(nums, target) {
     for(let i =0; i < nums.length; i++){
         for(let j = i +1; j < nums.length; j++){
             if(nums[i] + nums[j] == target){
-                // return the INDEX of i and j 
+                // return the INDEX -i and j 
                return [i,j];
            }
         }
@@ -167,3 +167,47 @@ ransomNote and magazine consist of lowercase English letters.
 */
 
 // HASHMAP
+
+
+
+/* 
+1762. Buildings With an Ocean View
+Medium
+
+724
+
+97
+
+Add to List
+
+Share
+There are n buildings in a line. You are given an integer array heights of size n that represents the heights of the buildings in the line.
+
+The ocean is to the right of the buildings. A building has an ocean view if the building can see the ocean without obstructions. Formally, a building has an ocean view if all the buildings to its right have a smaller height.
+
+Return a list of indices (0-indexed) of buildings that have an ocean view, sorted in increasing order.
+
+ 
+
+Example 1:
+
+Input: heights = [4,2,3,1]
+Output: [0,2,3]
+Explanation: Building 1 (0-indexed) does not have an ocean view because building 2 is taller.
+Example 2:
+
+Input: heights = [4,3,2,1]
+Output: [0,1,2,3]
+Explanation: All the buildings have an ocean view.
+Example 3:
+
+Input: heights = [1,3,2,4]
+Output: [3]
+Explanation: Only building 3 has an ocean view.
+ 
+
+Constraints:
+
+1 <= heights.length <= 105
+1 <= heights[i] <= 109
+*/
